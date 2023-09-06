8aabd1e22186493e286e39f97e48a2f9589e5be1
alejandroGomezLlanos <alejandro.gomez_lla@uao.edu.co>
2023-09-05 20:00:00 -0500

    arreglado lo de 2 imprimir, al quitar modo estricto

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import GiftExpertApp from './GiftExpertApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <GiftExpertApp />
-  </React.StrictMode>
 );
 
