d64a7f54aec003d4dcc7457b40a359d8507b60e5
alejandroGomezLlanos <alejandro.gomez_lla@uao.edu.co>
2023-09-28 19:55:21 -0500

    comit

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,11 +1,15 @@
-//segundo challenge
+//segundo 
 import {useState} from "react";
 import PropTypes from "prop-types"
+import {useFetch} from "./hooks/useFetch"
 
 const FirstApp = ({title, value}) => {
 	
 	//use state
 	const [counter, setCounter] = useState(value);
+
+	//mi custoom hook siendo invocado
+	const { data, isLoading, hasError } = useFetch(counter);
 
 	//funcion para agregar
 	const handleAdd = () => {
@@ -22,6 +26,18 @@ const FirstApp = ({title, value}) => {
 		setCounter(0);
 	}
 
+	//mi set cuando cambia el form
+	const onInputChange = (evt) => {
+		setCounter(evt.target.value);
+	}
+
+	//agrega url a estado y limpia el campo de texto
+	const onSubmit = (evt) => {
+		evt.preventDefault();
+		//onAddCategory(inputValue);
+		setCounter("");
+	}
+
 	return(
 	<>
 		<h1> {title} </h1>
@@ -30,9 +46,37 @@ const FirstApp = ({title, value}) => {
 		<button onClick={ () => handleAdd() } > Sumar 1 </button>
 		<button onClick={ () => handleSubstract() } > Restar 1 </button>
 		<button onClick={ () => handleReset() } > Reset </button>
+		<h1> llamando a useFetch </h1>
+		<h2> Escribe url aqui </h2>
+		<form onSubmit={(event => onSubmit(event))}>
+			<input
+				type="text"
+				placeholder="buscador"
+				value={counter}
+				onChange={(event) => onInputChange(event)}
+			/>
+		</form>
+		<h1> Resultados del custoom Hook </h1>
+		<ol>
+			{
+			data?.map(
+			(item, index) =>
+			{
+			return <li key={index}>{item.quote}</li>
+			}
+			)
+			}
+		</ol>
+		<h2> isLoading: {isLoading} </h2>
+		<h2> hasError: {hasError} </h2>
 	</>
 	);
 }
+	//correccion Jonathan
+	//llamar asi es llamarlo como componente, no como hooks
+	//un hook luce asi 
+	//const [counter, setCounter] = useState(value);
+	//<useFetch url={counter} />
 
 	//porp types, vamos a poner un por defecto
 	//porque quizas no nos envien en index el componente
@@ -45,7 +89,7 @@ const FirstApp = ({title, value}) => {
 	}
 
 	FirstApp.defaultProps = {
-		title: "Reto numero 2",
+		title: "Reto numero Boton API",
 		value: 0
 	}
 
