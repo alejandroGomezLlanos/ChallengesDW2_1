a70bad61863ba232847d02ab7c6a5bed0ba25a35
alejandroGomezLlanos <alejandro.gomez_lla@uao.edu.co>
2023-10-11 00:29:17 -0500

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Father} from './Father';
-
+//challenge 8
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Father />
