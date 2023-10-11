7788457636cc9b2f252662fb9ac6f93dc71614fc
alejandroGomezLlanos <alejandro.gomez_lla@uao.edu.co>
2023-10-11 00:29:52 -0500

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {TodoApp} from './TodoApp';
-
+//challenge 9
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <TodoApp />
