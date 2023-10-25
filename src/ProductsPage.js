7e23a95a1e5cf6b6b6b37fb250330d17bea77d96
alejandroGomezLlanos <alejandro.gomez_lla@uao.edu.co>
2023-10-24 19:49:49 -0500

    primer comit

--- src/ProductsPage.js
+++ src/ProductsPage.js
@@ -3,6 +3,14 @@ import {useParams} from "react-router-dom"
 
 export const ProductsPage = () => {
 	const {id} = useParams()
+	const {params} = useSearchParams()
+
+	const navigate = useNavigate()
+
+	const onLogout = () => {
+		navigate("/login")
+	}
+
 	return(
 		<>
 		<h1> El producto </h1>
